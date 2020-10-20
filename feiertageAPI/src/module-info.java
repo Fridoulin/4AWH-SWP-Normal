52a82eaf2679157541266f929b8a3f65e4632983
Niklas <59961104+Fridoulin@users.noreply.github.com>
2020-10-20 11:16:29 +0200

    Delete module-info.java

--- feiertageAPI/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module feiertageAPI {
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires java.json;
-    requires org.apache.commons.io;
-
-    opens sample;
-}
