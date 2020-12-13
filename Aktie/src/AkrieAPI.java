bff4333b8f7b7b1f5c9f0435cb81518ce6d3abc3
Niklas <59961104+Fridoulin@users.noreply.github.com>
2020-12-13 17:05:43 +0100

    Update AkrieAPI.java

--- Aktie/src/AkrieAPI.java
+++ Aktie/src/AkrieAPI.java
@@ -1,50 +1,77 @@
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javafx.application.Application;
+import java.util.Collections;
 
-public class AkrieAPI {
+
+public class AkrieAPI extends Application{
     static Scanner reader = new Scanner(System.in);
     static ArrayList<Double> closeWerte = new ArrayList<>();
+    static ArrayList<Double> gleitenderDurchschnitt = new ArrayList<>();
     static ArrayList<LocalDate> daten = new ArrayList<>();
     static String URL, auswahlAktie;
-    static int elemente = 100;
-
 
     public static void main (String args[]) throws IOException, JSONException {
     AkrieAPI a = new AkrieAPI();
     a.inputUser();
     a.readURL();
     a.getWert(URL);
+    a.durchschnitt();
     a.connect();
     a.createNewTable();
     a.insert();
+    a.insertAVG();
     a.selectAll();
-    a.gleitenderDurschnitt();
-   // System.out.println(daten);
-        // System.out.println(closeWerte);
+    Application.launch(args);
     }
     static void inputUser(){
         System.out.println("Aktie (nur USA): ");
         auswahlAktie = reader.next();
     }
     static void readURL () throws IOException, JSONException {
-        URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+auswahlAktie+ "&outputsize=compact&apikey=A0ZGRFDRZANZJGA8";
+        URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+auswahlAktie+ "&outputsize=full&apikey=A0ZGRFDRZANZJGA8";
     }
     static void getWert(String URL) throws JSONException, IOException {
         JSONObject json = new JSONObject(IOUtils.toString(new URL(URL), Charset.forName("UTF-8")));
         json = json.getJSONObject("Time Series (Daily)");
-            for(int i = 0; i < elemente; i++){
+            for(int i = 0; i < json.names().length(); i++){
                 daten.add(LocalDate.parse((CharSequence)json.names().get(i)));
                 closeWerte.add(json.getJSONObject(LocalDate.parse((CharSequence)json.names().get(i)).toString()).getDouble("4. close"));
             }
+    }
+
+    static void durchschnitt(){
+        int count = 0;
+        double wert = 0, avg;
+        for(int i = 0; i <= daten.size()-1; i++){
+            count++;
+            if(count <= 200){
+                wert = wert + closeWerte.get(i);
+                avg = wert/count;
+                gleitenderDurchschnitt.add(avg);
+            }
+            if(count > 200) {
+                wert = wert + closeWerte.get(i);
+                avg = wert/count;
+                gleitenderDurchschnitt.add(avg);
+            }
+        }
+
     }
     public static void connect() {
         Connection conn = null;
@@ -67,13 +94,14 @@ public class AkrieAPI {
     public static void createNewTable() {
         String url = "jdbc:sqlite:C:\\Users\\nisch\\IdeaProjects\\Aktie\\Aktie.db";
         String sql = "CREATE TABLE IF NOT EXISTS "+ auswahlAktie +" (\n"
-                + " date text, \n"
-                + " close double\n"
-                + ");";
+                + "datum text primary key, close real)";
+        String sqlAVG = "CREATE TABLE IF NOT EXISTS "+ auswahlAktie+"AVG (\n"
+                + "date text primary key, avg real)";
         try{
             Connection conn = DriverManager.getConnection(url);
             Statement stmt = conn.createStatement();
             stmt.execute(sql);
+            stmt.execute(sqlAVG);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -89,11 +117,11 @@ public class AkrieAPI {
         return conn;
     }
     public void insert() {
-        String sql = "INSERT OR REPLACE INTO "+auswahlAktie+" (date, close) VALUES(?,?)";
-        try{
+        String sql = "INSERT OR REPLACE INTO " + auswahlAktie + " (datum, close) VALUES(?, ?)";
+        try {
             Connection conn = this.connection();
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            for(int i = 0; i < elemente; i++) {
+            for (int i = 0; i < closeWerte.size(); i++) {
                 pstmt.setString(1, daten.get(i).toString());
                 pstmt.setDouble(2, closeWerte.get(i));
                 pstmt.executeUpdate();
@@ -102,30 +130,92 @@ public class AkrieAPI {
             System.out.println(e.getMessage());
         }
     }
+
+    public void insertAVG() {
+        String sqlAVG = "INSERT OR REPLACE INTO "+auswahlAktie+"AVG (avg, date) VALUES(?, ?)";
+            try{
+                Connection conn = this.connection();
+                PreparedStatement pstmt = conn.prepareStatement(sqlAVG);
+                for (int i = 0; i < daten.size(); i++) {
+                    pstmt.setDouble(1, gleitenderDurchschnitt.get(i));
+                    pstmt.setString(2, daten.get(i).toString());
+                    pstmt.executeUpdate();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     public void selectAll(){
-        String sql = "SELECT * FROM "+ auswahlAktie;
+        String sql = "SELECT * FROM "+ auswahlAktie +" order by datum";
+        String sqlAVG = "SELECT * FROM "+ auswahlAktie+"AVG order by date";
         try {
             Connection conn = this.connection();
-            Statement stmt  = conn.createStatement();
-            ResultSet rs    = stmt.executeQuery(sql);
+            Statement stmt = conn.createStatement();
+            Statement stmtAVG  = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            ResultSet rsAVG = stmtAVG.executeQuery(sqlAVG);
 
-            while (rs.next()) {
-                System.out.println("Datum           Close Werte");
+            System.out.println("Datum               Close Werte                 Durchschnitt");
+            while (rs.next() ) {
                 System.out.println(
-                        rs.getString("date") + "\t" + "\t" +
-                        rs.getDouble("close"));
+                        rs.getString("datum")  + "\t \t \t \t" +
+                                rs.getDouble("close") + "\t \t \t \t" +
+                                 rsAVG.getDouble("avg"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    public void gleitenderDurschnitt(){
-        double durchschnitt = 0, gleitenderDurchschnitt;
-        for(int i = 0; i < elemente; i++){
-            durchschnitt = durchschnitt + closeWerte.get(i);
+
+   @Override
+    public void start(Stage primaryStage) {
+        try {
+
+            final CategoryAxis xAxis = new CategoryAxis();
+            final NumberAxis yAxis = new NumberAxis();
+      //      ScrollPane scrollPane = new ScrollPane();             scrollbar
+            xAxis.setLabel("Datum");
+            yAxis.setLabel("close-Wert");
+            final LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
+            lineChart.setTitle("Aktienkurs");
+
+            XYChart.Series<String, Number> tatsaechlich = new XYChart.Series();
+            tatsaechlich.setName("Close-Werte");
+            for (int i = 0; i < daten.size()-1; i++) {
+                daten.sort(null);
+                if (gleitenderDurchschnitt.get(i) >= closeWerte.get(i)) {
+                    tatsaechlich.getNode().setStyle("-fx-stroke: #15ff00; ");
+                }
+                if(gleitenderDurchschnitt.get(i) < closeWerte.get(i)) {
+                    tatsaechlich.getNode().setStyle("-fx-stroke: #ff0000; ");
+                }
+                tatsaechlich.getData().add(new XYChart.Data(daten.get(i).toString(), closeWerte.get(i)));
+                }
+
+            Collections.reverse(gleitenderDurchschnitt);
+            XYChart.Series<String, Number> durchschnitt = new XYChart.Series();
+            durchschnitt.setName("gleitender Durchschnitt");
+            for (int i = 0; i < gleitenderDurchschnitt.size()-1; i++) {
+                daten.sort(null);
+                    durchschnitt.getData().add(new XYChart.Data(daten.get(i).toString(), gleitenderDurchschnitt.get(i)));
+            }
+
+            Scene scene = new Scene(lineChart, 800, 600);
+            lineChart.getData().add(tatsaechlich);
+            lineChart.getData().add(durchschnitt);
+
+            for(int i = 0; i < gleitenderDurchschnitt.size(); i++) {
+
+            }
+            lineChart.setCreateSymbols(false);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
-        gleitenderDurchschnitt = durchschnitt/elemente;
-        System.out.println("");
-        System.out.println("gleitender Durchschnitt:" + gleitenderDurchschnitt);
     }
 }
+
+
+
