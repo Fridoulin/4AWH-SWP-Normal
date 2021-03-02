0edf2d78f3ac20db63f95cff7e268f74a3d0cdf1
Niklas <59961104+Fridoulin@users.noreply.github.com>
2021-03-02 10:16:42 +0100

    Update AktieAPI.java

--- Aktie/src/AktieAPI.java
+++ Aktie/src/AktieAPI.java
@@ -1,12 +1,16 @@
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -16,6 +20,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 import javafx.application.Application;
+
+import javax.imageio.ImageIO;
 
 public class AktieAPI extends Application{
     static Scanner reader = new Scanner(System.in);
@@ -35,11 +41,12 @@ public class AktieAPI extends Application{
         a.getWert(URL);
         a.connect();
         a.createNewTable();
-        a.durchschnitt();
         a.insert();
+        a.durchschnitt();
         a.insertAVG();
         a.selectAll();
         Application.launch(args);
+
     }
     static void inputUser() {
         System.out.println("Aktie (nur USA): ");
@@ -50,7 +57,7 @@ public class AktieAPI extends Application{
         avgauswahl = reader.nextInt();
     }
     static void readURL() {
-        URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+auswahlAktie+"&outputsize="+type+"&apikey=KEY"; //alphavantage-schüssel einfügen
+        URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+auswahlAktie+"&outputsize="+type+"&apikey=A0ZGRFDRZANZJGA8";
     }
     static void getWert(String URL) throws JSONException, IOException {
         JSONObject json = new JSONObject(IOUtils.toString(new URL(URL), Charset.forName("UTF-8")));
@@ -94,7 +101,7 @@ public class AktieAPI extends Application{
     public static void connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:PFAD"; //Pfad einfügen
+            String url = "jdbc:sqlite:C:\\Users\\nisch\\IdeaProjects\\Aktie\\Aktie.db"; //Pfad einfügen
             conn = DriverManager.getConnection(url);
             System.out.println("Connection to SQLite has been established.");
         } catch (SQLException e) {
@@ -110,7 +117,7 @@ public class AktieAPI extends Application{
         }
     }
     public static void createNewTable() {
-        String url = "jdbc:sqlite:PFAD"; //Pfad einfügen
+        String url = "jdbc:sqlite:C:\\Users\\nisch\\IdeaProjects\\Aktie\\Aktie.db"; //Pfad einfügen
         String sql = "CREATE TABLE IF NOT EXISTS "+ auswahlAktie +" (\n"
                 + "datum text primary key, close real)";
         String sqlAVG = "CREATE TABLE IF NOT EXISTS "+ auswahlAktie+"AVG (\n"
@@ -125,7 +132,7 @@ public class AktieAPI extends Application{
         }
     }
     private Connection connection() {
-        String url = "jdbc:sqlite:PFAD"; //Pfad einfügen
+        String url = "jdbc:sqlite:C:\\Users\\nisch\\IdeaProjects\\Aktie\\Aktie.db"; //Pfad einfügen
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -139,7 +146,7 @@ public class AktieAPI extends Application{
         try {
             Connection conn = this.connection();
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            for (int i = 0; i < closeWerte.size(); i++) {
+            for (int i = 0; i < closeWerte.size()-1; i++) {
                 pstmt.setString(1, daten.get(i).toString());
                 pstmt.setDouble(2, closeWerte.get(i));
                 pstmt.executeUpdate();
@@ -149,7 +156,7 @@ public class AktieAPI extends Application{
         }
     }
     public void insertAVG() {
-        String sqlAVG = "INSERT OR REPLACE INTO "+auswahlAktie+"AVG (date, avg) VALUES(?, ?)";
+        String sqlAVG = "INSERT OR REPLACE INTO "+ auswahlAktie+"AVG (date, avg) VALUES(?, ?)";
         try{
             Connection conn = this.connection();
             PreparedStatement pstmt = conn.prepareStatement(sqlAVG);
@@ -171,19 +178,16 @@ public class AktieAPI extends Application{
             Statement stmtAVG  = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             ResultSet rsAVG = stmtAVG.executeQuery(sqlAVG);
-
             System.out.println("Datum               Close Werte             Durchschnitt");
             while (rs.next() && rsAVG.next()) {
                 System.out.println(
                         rs.getString("datum")  + "\t \t \t \t" +
                                 rs.getDouble("close") + "\t \t \t \t" +
-                                rsAVG.getDouble("avg")
-                );
+                                rsAVG.getDouble("avg"));
                 dateDB.add(rsAVG.getString("date"));
                 avgDB.add(rsAVG.getDouble("avg"));
+                dateDB.sort(null);
             }
-            dateDB.sort(null);
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -201,12 +205,13 @@ public class AktieAPI extends Application{
             lineChart.setTitle("Aktienkurs " + auswahlAktie);
             XYChart.Series<String, Number> tatsaechlich = new XYChart.Series();
             tatsaechlich.setName("Close-Werte");
-            for (int i = 0; i < closeWerte.size() - 1; i++) {
+
+            for (int i = 0; i < dateDB.size() - 1; i++) {
                 tatsaechlich.getData().add(new XYChart.Data(dateDB.get(i), closeDB.get(i)));
             }
             XYChart.Series<String, Number> durchschnitt = new XYChart.Series();
             durchschnitt.setName("gleitender Durchschnitt");
-            for (int i = 0; i < gleitenderDurchschnitt.size() - 1; i++) {
+            for (int i = 0; i < dateDB.size() - 1; i++) {
                 durchschnitt.getData().add(new XYChart.Data(dateDB.get(i), avgDB.get(i)));
             }
             Scene scene = new Scene(lineChart, 1000, 600);
@@ -219,16 +224,20 @@ public class AktieAPI extends Application{
             yAxis.setUpperBound(verschiebenOben+20);
 
             if (closeWerte.get(closeWerte.size()-1) >= avgDB.get(avgDB.size()-1)) {
-            scene.getStylesheets().add("backgroundRed.css");
+                scene.getStylesheets().add("backgroundRed.css");
             }
             if (closeWerte.get(closeWerte.size()-1) < avgDB.get(avgDB.size()-1)) {
                 scene.getStylesheets().add("backgroundGreen.css");
             }
-
+            tatsaechlich.getNode().setStyle("-fx-stroke: #000000; ");
+            durchschnitt.getNode().setStyle("-fx-stroke: #ffffff; ");
             lineChart.setCreateSymbols(false);
             primaryStage.setScene(scene);
             primaryStage.show();
-
+            WritableImage image = scene.snapshot(null);
+            File file = new File("C:\\Users\\nisch\\IdeaProjects\\Aktie\\Image\\"+auswahlAktie+" "+LocalDate.now()+".png");
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "PNG", file);
+            System.out.println("Image Saved");
         } catch(Exception e) {
             e.printStackTrace();
         }
