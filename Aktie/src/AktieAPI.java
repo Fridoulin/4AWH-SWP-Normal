f4333e3e6fb92e3a319ea8da1712c60bd1ccccd1
Niklas <59961104+Fridoulin@users.noreply.github.com>
2021-05-14 17:39:44 +0200

    splitcorrection

--- Aktie/src/AktieAPI.java
+++ Aktie/src/AktieAPI.java
@@ -30,10 +30,11 @@ public class AktieAPISQL extends Application{
     static ArrayList<Double> closeWerte = new ArrayList<>();
     static ArrayList<Double> gleitenderDurchschnitt = new ArrayList<>();
     static ArrayList<String> daten = new ArrayList<>();
-    static ArrayList<Double> closeDB = new ArrayList<>();
     static ArrayList<String> dateDB = new ArrayList<>();
     static ArrayList<Double> adjustedSplit = new ArrayList<>();
     static ArrayList<String> auswahlAktie = new ArrayList<>();
+    static ArrayList<Double> adjustedCoefficient = new ArrayList<>();
+
     static String URL, type, key, verzeichnis, aktienDB;
     static int avgauswahl;
 
@@ -57,7 +58,7 @@ public class AktieAPISQL extends Application{
     }
     static void inputUser() throws IOException {
         try {
-            File file = new File("C:\\Users\\IdeaProjects\\Aktie\\src\\aktien.txt"); //Pfad
+            File file = new File("C:\\Users\\nisch\\IdeaProjects\\AktieAPISQL\\src\\aktien.txt"); //Pfad
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
             key = br.readLine();
@@ -92,10 +93,11 @@ public class AktieAPISQL extends Application{
             System.out.println("Es wurde noch kein Tabel angelegt");
         }
     }
+
     static boolean createTable(String tempAktie) throws SQLException {
         try{
             myStmt = connection.createStatement();
-            String createtable = "create table if not exists "+tempAktie+" (datum varchar(255) primary key, close double, split double);";
+            String createtable = "create table if not exists "+tempAktie+"_Roh (datum varchar(255) primary key, close double, split double);";
             myStmt.executeUpdate(createtable);
             return true;
         } catch (SQLException e) {
@@ -103,77 +105,118 @@ public class AktieAPISQL extends Application{
         }
         return false;
     }
+    static boolean createTableClac(String tempAktie) throws SQLException{
+        try{
+            myStmt = connection.createStatement();
+            String createtable = "create table if not exists "+tempAktie+"_Calc (datum varchar(255) primary key, closeCorrect double, avg double );";
+            myStmt.executeUpdate(createtable);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     static void getWert(String URL) throws JSONException, IOException {
         JSONObject json = new JSONObject(IOUtils.toString(new URL(URL), Charset.forName("UTF-8")));
         json = json.getJSONObject("Time Series (Daily)");
         for (int i = 0; i < json.names().length(); i++) {
             daten.add(LocalDate.parse((CharSequence) json.names().get(i)).toString());
             closeWerte.add(json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString()).getDouble("4. close"));
-            adjustedSplit.add(json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString()).getDouble("5. adjusted close"));
+            adjustedCoefficient.add(json.getJSONObject(LocalDate.parse((CharSequence) json.names().get(i)).toString()).getDouble("8. split coefficient"));
         }
     }
-    static void clear(){
+
+    static void clear() {
         daten.clear();
         closeWerte.clear();
+        adjustedCoefficient.clear();
         adjustedSplit.clear();
-        closeDB.clear();
         dateDB.clear();
         gleitenderDurchschnitt.clear();
     }
-    static void writeDataInDB(String tempAktie){
+
+    static void writeDataInDB(String tempAktie) {
         try {
-            myStmt = connection.createStatement();
-            for(int i = 0; i < daten.size(); i++) {
-                String writeData = "insert ignore into " + tempAktie + " (datum, close, split) values('" + daten.get(i) + "', '" + closeWerte.get(i) + "', '" + adjustedSplit.get(i) + "');";
+            for (int i = 0; i < daten.size(); i++) {
+                String writeData = "insert ignore into " + tempAktie+"_Roh (datum, close, split) values('" + daten.get(i) + "', '" + closeWerte.get(i) + "', '" + adjustedCoefficient.get(i) + "');";
                 myStmt.executeUpdate(writeData);
             }
-            System.out.println("Datensatz eingetragen");
-        }
-        catch (SQLException e) {
+            System.out.println("Rohdaten eingetragen");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    static void durchschnitt(String tempAktie) {
+
+    static void splitCorrection(String tempAktie) {
+        ArrayList<Double> coe = new ArrayList<>();
+        ArrayList<Double> close = new ArrayList<>();
+        double coefficient = 1.0;
+        double temp;
         try {
-            Statement myStmt = connection.createStatement();
-            String querry = "SELECT * from " + tempAktie;
+            String querry = "SELECT * from " + tempAktie+"_Roh order by datum asc;";
             ResultSet rs = myStmt.executeQuery(querry);
             while (rs.next()) {
-                closeDB.add(rs.getDouble("split"));
+                close.add(rs.getDouble("close"));
+                coe.add(rs.getDouble("split"));
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        for (int i = 0; i < coe.size(); i++) {
+            coefficient *= coe.get(i);
+            temp = coefficient * close.get(i);
+            adjustedSplit.add((double) Math.round(temp * 100) / 100);
+        }
+        coe.clear();
+        close.clear();
 
+    }
+
+    static void durchschnitt(String tempAktie) {
         int count = 0;
-        double wert = 0, x,avg;
-        for(int i = 0; i <= closeDB.size()-1; i++){
+        double wert = 0, x, avg;
+        for (int i = 0; i <= adjustedSplit.size() - 1; i++) {
             count++;
-            if(count <= avgauswahl){
-                wert = wert + closeDB.get(i);
-                avg = wert/count;
+            if (count <= avgauswahl) {
+                wert = wert + adjustedSplit.get(i);
+                avg = wert / count;
                 gleitenderDurchschnitt.add(avg);
             }
-            if(count > avgauswahl) {
-                x = closeDB.get(i-avgauswahl);
+            if (count > avgauswahl) {
+                x = adjustedSplit.get(i - avgauswahl);
                 wert = wert - x;
-                wert = wert + closeDB.get(i);
-                avg = wert/avgauswahl;
+                wert = wert + adjustedSplit.get(i);
+                avg = wert / avgauswahl;
                 gleitenderDurchschnitt.add(avg);
             }
+        }
+    }
+
+    static void writeCorrectDataInDB(String tempAktie) {
+        try {
+            if(adjustedSplit.size() == 0){
+                splitCorrection(tempAktie);
+            }
+            if(gleitenderDurchschnitt.size() == 0){
+                durchschnitt(tempAktie);
+            }
+            for (int i = 0; i < daten.size(); i++) {
+                String writeData = "insert ignore into " + tempAktie+"_Calc (datum, closeCorrect, avg) values('" + daten.get(i) + "', '" + adjustedSplit.get(i) + "', '" + gleitenderDurchschnitt.get(i) + "');";
+                myStmt.executeUpdate(writeData);
+            }
+            System.out.println("Datensatz eingetragen");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     public static void getData(String tempAktie) {
         try {
             Statement myStmt = connection.createStatement();
-            ResultSet rsNormal = myStmt.executeQuery( "SELECT * from " + tempAktie);
-            System.out.println("Datum               Close Werte");
+            ResultSet rsNormal = myStmt.executeQuery( "SELECT * from " + tempAktie+"_Calc");
             while (rsNormal.next()) {
-             /*   System.out.println(
-                        rsNormal.getString("datum") + "\t \t \t \t" +
-                                rsNormal.getDouble("split"));*/
+
                 dateDB.add(rsNormal.getString("datum"));
-                closeDB.add(rsNormal.getDouble("split"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -193,9 +236,12 @@ public class AktieAPISQL extends Application{
                 readURL(tempAktie);
                 selectToCheck(tempAktie);
                 createTable(tempAktie);
+                createTableClac(tempAktie);
                 getWert(URL);
+                splitCorrection(tempAktie);
                 durchschnitt(tempAktie);
                 writeDataInDB(tempAktie);
+                writeCorrectDataInDB(tempAktie);
                 getData(tempAktie);
 
                 final CategoryAxis xAxis = new CategoryAxis();
@@ -211,7 +257,7 @@ public class AktieAPISQL extends Application{
                 XYChart.Series<String, Number> durchschnitt = new XYChart.Series();
                 tatsaechlich.setName("Close-Werte");
                 for (int i = 0; i < dateDB.size() - 1; i++) {
-                    tatsaechlich.getData().add(new XYChart.Data(dateDB.get(i), closeDB.get(i)));
+                    tatsaechlich.getData().add(new XYChart.Data(dateDB.get(i), adjustedSplit.get(i)));
                 }
                 durchschnitt.setName("gleitender Durchschnitt");
                 for (int i = 0; i < gleitenderDurchschnitt.size()-1; i++) {
@@ -220,15 +266,15 @@ public class AktieAPISQL extends Application{
                 lineChart.getData().add(tatsaechlich);
                 lineChart.getData().add(durchschnitt);
                 yAxis.setAutoRanging(false);
-                double verschiebenOben = Collections.max(closeDB);
-                double verschiebenUnten = Collections.min(closeDB);
+                double verschiebenOben = Collections.max(adjustedSplit);
+                double verschiebenUnten = Collections.min(adjustedSplit);
                 yAxis.setLowerBound(verschiebenUnten - 20);
                 yAxis.setUpperBound(verschiebenOben + 20);
                 tatsaechlich.getNode().setStyle("-fx-stroke: #000000; ");
                 durchschnitt.getNode().setStyle("-fx-stroke: #ffffff; ");
                 lineChart.setCreateSymbols(false);
-                if(closeDB.size() > 0 && gleitenderDurchschnitt.size() > 0){
-                    if(closeDB.get(closeDB.size()-1) > gleitenderDurchschnitt.get(gleitenderDurchschnitt.size()-1)) {
+                if(adjustedSplit.size() > 0 && gleitenderDurchschnitt.size() > 0){
+                    if(adjustedSplit.get(adjustedSplit.size()-1) > gleitenderDurchschnitt.get(gleitenderDurchschnitt.size()-1)) {
                         scene.getStylesheets().add("backgroundGreen.css");
                     }else {
                         scene.getStylesheets().add("backgroundRed.css");
